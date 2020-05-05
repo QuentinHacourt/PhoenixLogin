dc648b1d94941d9ddb5f44cfe4baaee89b5b24cc
quentinhacourt <quentin.hacourt@student.ucll.be>
2020-05-05 21:25:06 +0200

    first commit

---
+++ lib/chat_app_web/router.ex
@@ -0,0 +1,43 @@
+defmodule ChatAppWeb.Router do
+  use ChatAppWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", ChatAppWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+    resources "/users", UserController
+
+    resources "/sessions", SessionController,
+      only: [:new, :create, :delete],
+      singleton: true
+  end
+
+  defp authenticate_user(conn, _) do
+    case get_session(conn, :user_id) do
+      nil ->
+        conn
+        |> Phoenix.Controller.put_flash(:error, "Login required")
+        |> Phoenix.Controller.redirect(to: "/")
+        |> halt()
+      user_id ->
+        assign(conn, :current_user, Hello.Accounts.get_user!(user_id))
+    end
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", ChatAppWeb do
+  #   pipe_through :api
+  # end
+end
